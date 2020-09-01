6b13806b83fadd24f43b69c61628e52156d71747
toutpt <toutpt@gmail.com>
2020-09-01 09:15:04 +0200

    docs: publish

---
+++ docs/precache-manifest.7e56a7ab08c42c1c99b32f36e1546a67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "751c2cd5e62926cdc5636ccbdda90d0c",
+    "url": "/solfege/index.html"
+  },
+  {
+    "revision": "1e548406445060803668",
+    "url": "/solfege/static/css/main.489138d9.chunk.css"
+  },
+  {
+    "revision": "3ff26826cf40cb9b5d9a",
+    "url": "/solfege/static/js/2.383c5247.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/solfege/static/js/2.383c5247.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e548406445060803668",
+    "url": "/solfege/static/js/main.01895223.chunk.js"
+  },
+  {
+    "revision": "12da0611f85a05f4ab33",
+    "url": "/solfege/static/js/runtime-main.ae954399.js"
+  }
+]);
